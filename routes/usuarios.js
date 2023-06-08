8148fd6e9d1cb653431ce744778711ab30c2b5bd
Keila19JH <keila19gutierrez@gmail.com>
2023-06-07 21:25:08 -0600

    primer commit

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,6 +1,26 @@
 const {Router, response, request} = require('express');
+const bcryptjs = require('bcryptjs');
+
+
 const router = Router();
 
+
+//crear usuarios: post 
+router.post('/', (req = request, res = response) => {
+
+    const { password, ...data } = req.body;
+
+    const salt = bcryptjs.genSaltSync(11);
+    const newPassword = bcryptjs.hashSync( `${data.password}`, salt ) //codificacion de contraseña
+
+   const newUser = {
+        mesagge: 'Usuario creado satisfactoriamente',
+        data
+   }
+
+   res.status(200).json( newUser );
+});
+//res : operador res, sacar el resto de info de objetos y arreglos en otro nuevo
 //obtener usuarios:   get
 
 router.get('/', (req = request, res = response) => {
